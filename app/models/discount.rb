b7c0352c43c990cff80ed6c8814cda62b3490266
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-12-15 21:22:08 -0200

    improvements in fae models and views

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,9 +1,9 @@
 class Discount < ApplicationRecord
   include Fae::BaseModelConcern
+  validates :value, uniqueness: true, presence: true
+  validates :title, uniqueness: true, presence: true
 
   def fae_display_field
     title
   end
-
-
 end
